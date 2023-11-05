c90eb1d45c93dc3f936d56845794280c181d2da2
EMMETT-DEV <126395514+EMMETT-DEV@users.noreply.github.com>
2023-11-05 18:03:43 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+// your-script.js
+document.addEventListener("DOMContentLoaded", function() {
+     //JavaScript code that runs after the HTML document has loaded.
+    var heading = document.querySelector("h1");
+    heading.style.color = "blue";
+});
+console.log("hello, world!")
